313ad908564acbff5aacd84796b3a8a463bb33bf
AmandaGQuiocho <amanda.quiocho@smc.pshs.edu.ph>
2026-04-21 22:09:23 +0800

    Update G7_Room1_PD4.java

--- codes/G7_Room1_PD4.java
+++ codes/G7_Room1_PD4.java
@@ -1,12 +1,14 @@
+package codes;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class SimpleMazeGame extends JPanel implements KeyListener {
+public class G7_Room1_PD4 extends JPanel implements KeyListener {
 
     final int TILE = 40;
 
-    Image playerImg, wallImg, npcImg, enemyImg, floorImg;
+    Image G7_player, G7_wall, G7_npc, G7_enemy, G7_floor;
 
     String[] maze = {
             "111111111111",
@@ -25,22 +27,49 @@ public class SimpleMazeGame extends JPanel implements KeyListener {
     int playerCol = 1;
 
     boolean talkedToNPC = false;
-    boolean gameFinished = false;
+    boolean hasKey = false;
+    boolean doorUnlocked = false;
 
     int playerHP = 100;
-    int enemyHP = 100;
 
-    public SimpleMazeGame() {
+    JFrame frame;
 
+    public G7_Room1_PD4() {
         setPreferredSize(new Dimension(cols * TILE, rows * TILE + 70));
         setFocusable(true);
         addKeyListener(this);
 
-        playerImg = new ImageIcon(getClass().getResource("/assets/player.png")).getImage();
-        wallImg   = new ImageIcon(getClass().getResource("/assets/wall.png")).getImage();
-        npcImg    = new ImageIcon(getClass().getResource("/assets/npc.png")).getImage();
-        enemyImg  = new ImageIcon(getClass().getResource("/assets/enemy.png")).getImage();
-        floorImg  = new ImageIcon(getClass().getResource("/assets/floor.png")).getImage();
+        G7_player = new ImageIcon(getClass().getResource("/assets/player.png")).getImage();
+        G7_wall   = new ImageIcon(getClass().getResource("/assets/wall.png")).getImage();
+        G7_npc    = new ImageIcon(getClass().getResource("/assets/npc.png")).getImage();
+        G7_enemy  = new ImageIcon(getClass().getResource("/assets/enemy.png")).getImage();
+        G7_floor  = new ImageIcon(getClass().getResource("/assets/floor.png")).getImage();
+
+        loadSaveData();
+    }
+
+    private void loadSaveData() {
+        SaveSystem.SaveData save = SaveSystem.loadGame("G7_Room1_PD4");
+        SaveSystem.startTimer(save.timeSeconds);
+
+        talkedToNPC = save.hasFlag("talked_npc");
+        hasKey = save.hasFlag("has_key");
+        doorUnlocked = save.hasFlag("door_unlocked");
+
+        System.out.println("[G7 Room1] Loaded - talkedToNPC: " + talkedToNPC
+                + " hasKey: " + hasKey
+                + " doorUnlocked: " + doorUnlocked
+                + " time: " + save.formattedTime());
+    }
+
+    private void saveProgress() {
+        SaveSystem.saveGame(
+                new SaveSystem.SaveData.Builder("G7_Room1_PD4")
+                        .flag(talkedToNPC ? "talked_npc" : null)
+                        .flag(hasKey ? "has_key" : null)
+                        .flag(doorUnlocked ? "door_unlocked" : null)
+                        .battles(SaveSystem.getDefeatedBosses())
+        );
     }
 
     @Override
@@ -53,104 +82,98 @@ public class SimpleMazeGame extends JPanel implements KeyListener {
                 char tile = maze[r].charAt(c);
 
                 if (tile == '1') {
-                    g.drawImage(wallImg, c * TILE, r * TILE, TILE, TILE, null);
+                    g.drawImage(G7_wall, c * TILE, r * TILE, TILE, TILE, null);
                 } else {
-                    g.drawImage(floorImg, c * TILE, r * TILE, TILE, TILE, null);
+                    g.drawImage(G7_floor, c * TILE, r * TILE, TILE, TILE, null);
                 }
 
                 if (tile == 'N') {
-                    g.drawImage(npcImg, c * TILE, r * TILE, TILE, TILE, null);
+                    g.drawImage(G7_npc, c * TILE, r * TILE, TILE, TILE, null);
                 }
 
                 if (tile == 'E') {
-                    g.drawImage(enemyImg, c * TILE, r * TILE, TILE, TILE, null);
+                    g.drawImage(G7_enemy, c * TILE, r * TILE, TILE, TILE, null);
                 }
             }
         }
 
-        g.drawImage(playerImg, playerCol * TILE, playerRow * TILE, TILE, TILE, null);
+        g.drawImage(G7_player, playerCol * TILE, playerRow * TILE, TILE, TILE, null);
 
         g.setColor(Color.BLACK);
         g.drawString("Player HP: " + playerHP, 20, rows * TILE + 25);
-        g.drawString("Enemy HP: " + enemyHP, 160, rows * TILE + 25);
+        g.drawString("Key: " + (hasKey ? "YES" : "NO"), 140, rows * TILE + 25);
+        g.drawString("Door: " + (doorUnlocked ? "UNLOCKED" : "LOCKED"), 240, rows * TILE + 25);
     }
 
     void movePlayer(int dr, int dc) {
-
-        if (gameFinished) return;
-
         int newRow = playerRow + dr;
         int newCol = playerCol + dc;
 
+        if (newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols) return;
         if (maze[newRow].charAt(newCol) == '1') return;
+
+        char tile = maze[newRow].charAt(newCol);
+
+        if (tile == 'E' && !doorUnlocked) {
+            JOptionPane.showMessageDialog(this,
+                    "The door is locked.\nTalk to the NPC first.");
+            return;
+        }
 
         playerRow = newRow;
         playerCol = newCol;
 
-        char tile = maze[newRow].charAt(newCol);
-
         if (tile == 'N' && !talkedToNPC) {
             talkedToNPC = true;
+            hasKey = true;
+            doorUnlocked = true;
             playerHP += 20;
 
             JOptionPane.showMessageDialog(this,
-                    "NPC: Take this help (+20 HP)\nPlayer HP: " + playerHP);
+                    "NPC: Take this help (+20 HP)\n" +
+                    "You received the key.\n" +
+                    "The door is now unlocked!");
+
+            saveProgress();
         }
 
-        if (tile == 'E' && !gameFinished) {
-            startBattle();
+        if (tile == 'E' && doorUnlocked) {
+            transitionToPD6();
+            return;
         }
 
         repaint();
     }
 
-    void startBattle() {
-
-        enemyHP = 100;
+    void transitionToPD6() {
+        saveProgress();
 
         JOptionPane.showMessageDialog(this,
-                "Enemy Appears! Battle Start!");
+                "You unlocked the door!\nProceeding to Room 2...");
 
-        while (playerHP > 0 && enemyHP > 0) {
-
-            String q = JOptionPane.showInputDialog(this,
-                    "What is 3 + 3?");
-
-            if ("6".equals(q)) {
-                enemyHP -= 30;
-                playerHP += 10;
-
-                JOptionPane.showMessageDialog(this,
-                        "Correct! Enemy -30 HP");
-            } else {
-                playerHP -= 30;
-
-                JOptionPane.showMessageDialog(this,
-                        "Wrong! You take damage!");
-            }
-
-            repaint();
-
-            if (enemyHP <= 0 || playerHP <= 0) break;
+        Window window = SwingUtilities.getWindowAncestor(this);
+        if (window instanceof JFrame currentFrame) {
+            currentFrame.dispose();
         }
 
-        if (playerHP <= 0) {
-            JOptionPane.showMessageDialog(this, "You Lost!");
-            System.exit(0);
-        } else {
-            JOptionPane.showMessageDialog(this, "You Win!");
-            gameFinished = true;
-        }
+        JFrame nextFrame = new JFrame("Room 2 - G7");
+        G7_Room2_PD6 nextRoom = new G7_Room2_PD6();
+        nextRoom.frame = nextFrame;
+
+        nextFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        nextFrame.add(nextRoom);
+        nextFrame.pack();
+        nextFrame.setLocationRelativeTo(null);
+        nextFrame.setVisible(true);
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-
         switch (e.getKeyCode()) {
-            case KeyEvent.VK_UP -> movePlayer(-1, 0);
-            case KeyEvent.VK_DOWN -> movePlayer(1, 0);
-            case KeyEvent.VK_LEFT -> movePlayer(0, -1);
-            case KeyEvent.VK_RIGHT -> movePlayer(0, 1);
+            case KeyEvent.VK_UP, KeyEvent.VK_W -> movePlayer(-1, 0);
+            case KeyEvent.VK_DOWN, KeyEvent.VK_S -> movePlayer(1, 0);
+            case KeyEvent.VK_LEFT, KeyEvent.VK_A -> movePlayer(0, -1);
+            case KeyEvent.VK_RIGHT, KeyEvent.VK_D -> movePlayer(0, 1);
         }
     }
 
@@ -158,9 +181,13 @@ public class SimpleMazeGame extends JPanel implements KeyListener {
     @Override public void keyTyped(KeyEvent e) {}
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Simple Maze RPG");
+        JFrame frame = new JFrame("Room 1 - G7");
+        G7_Room1_PD4 panel = new G7_Room1_PD4();
+
+        panel.frame = frame;
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new SimpleMazeGame());
+        frame.add(panel);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
